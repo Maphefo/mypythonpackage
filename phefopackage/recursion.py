a4692dabc3d0c713461c705ee23b3a1ddea498c6
Mmaphefo Seseni <Mmaphefo@explore-datascience.net>
2019-03-22 16:32:37 +0200

    modified recursion cod

--- phefopackage/recursion.py
+++ phefopackage/recursion.py
@@ -1,28 +1,52 @@
 def sum_array(array):
 
-    '''Return sum of all items in array'''
-    return array.sum()
+   '''Return sum of all items in array'''
+
+   if len(array) == 1:
+       return array[0]
+   else:
+       return array[0] + sum_array(array[1:])
 
 def fibonacci(n):
 
-    '''Return nth term in fibonacci sequence'''
-    if n <=1:
-        return n
-    else:
-        return n+(n-1)
+   """
+   Calculate nth term in fibonacci sequence
+
+   Args:
+       n (int): nth term in fibonacci sequence to calculate
+
+   Returns:
+       int: nth term of fibonacci sequence,
+            equal to sum of previous two terms
+   """
+
+   if n <= 1:
+       return n
+   if n == 2:
+       return 1
+   else:
+       return fibonacci(n - 1) + fibonacci(n - 2)
 
 def factorial(n):
 
-    '''Return n!'''
-    result = 1
-    count = 2
+   '''Returns the product of all positive integers less than or equal to n.'''
 
-    while count <= n:
-        result = result * count
-        count = count + 1
-    return result
+   if n < 0:
+       raise ValueError(" n must be a positive integer")
+
+   elif n == 0:
+       return 1
+
+   elif n == 1:
+       return n
+
+   else:
+       return  n * factorial(n-1)
 
 def reverse(word):
 
-    '''Return word in reverse'''
-    return word[::-1]
+   '''Return word in reverse'''
+   if len(word) > 1:
+       return word[-1] + reverse(word[:-1])
+   else:
+       return word
--- phefopackage/sorting.py
+++ phefopackage/sorting.py
@@ -1,28 +1,50 @@
 def bubble_sort(items):
 
-    '''Return array of items, sorted in ascending order'''
-    out = items.copy() # in place protection on items
-    for i in range(len(out)):
-        for j in range(len(out)-1-i):
-            if out[j] > out[j+1]:
-                out[j], out[j+1] = out[j+1], out[j]     # Swap!
+   '''Return array of items, sorted in ascending order'''
 
-    return out
+   bubbled_items = items.copy() # Making sure that the new list is of the same length as items
+   for i in range(len(bubbled_items)):
+       for j in range(len(bubbled_items)-1-i):
+           if bubbled_items[j] > bubbled_items[j+1]:
+               bubbled_items[j], bubbled_items[j+1] = bubbled_items[j+1], bubbled_items[j]
+
+   return bubbled_items
 
 def merge_sort(items):
+   """ Return array of items, sorted in ascending order """
+   if len(items) >1:
+       mid = len(items)//2 #Finding the mid of the array
+       L = items[:mid] # Dividing the array elements
+       R = items[mid:] # into 2 halves
 
-    '''Return array of items, sorted in ascending order'''
-    len_i = len(items)
-    if len_i == 1:
-        return items
+       merge_sort(L) # Sorting the first half
+       merge_sort(R) # Sorting the second half
 
-    mid_point = int(len_i / 2)
-    i1 = merge_sort(items[:mid_point])
-    i2 = merge_sort(items[mid_point:])
+       i = j = k = 0
 
-    return merge(i1, i2)
+       # Copy data to temp arrays L[] and R[]
+       while i < len(L) and j < len(R):
+           if L[i] < R[j]:
+               items[k] = L[i]
+               i+=1
+           else:
+               items[k] = R[j]
+               j+=1
+           k+=1
+
+       # Checking if any element was left
+       while i < len(L):
+           items[k] = L[i]
+           i+=1
+           k+=1
+
+       while j < len(R):
+           items[k] = R[j]
+           j+=1
+           k+=1
+   return items
 
 def quick_sort(items):
-
-    '''Return array of items, sorted in ascending order'''
-    return items.sort()
+   """ Return array of items, sorted in ascending order """
+   return quick_sort([i for i in items[1:] if i <= items[0]]) + [items[0]] \
+       + quick_sort([i for i in items[1:] if i >= items[0]]) if items else []
