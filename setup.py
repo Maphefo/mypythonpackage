61994c9dbab535c33c840639532ce06e3bccb476
Mmaphefo Seseni <Mmaphefo@explore-datascience.net>
2019-03-22 15:53:35 +0200

    modified setup.py file

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/Maphefo/mypackage',
+    url='https://github.com/Maphefo/phefopackage.git',
     author='Mmaphefo Seseni',
     author_email='michel.seseni@gmail.com'
 )
